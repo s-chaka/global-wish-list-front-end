f85c48e17bc207df81042e690d5080db82bd4a85
Selam Chaka <selblessed@gmail.com>
2023-01-25 19:16:02 -0500

    adding .env, signupform, signinform, users.py files

--- global-wish-list/src/App.js
+++ global-wish-list/src/App.js
@@ -1,7 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const URL = process.env['REACT_APP_BACKEND_URL'];
+
+const App = () =>{
+  const [userData, setUserData] = useState([]);
+
+
+  useEffect(() => {
+    axios 
+      .get(URL)
+      .then((response)=> {
+        const newUsers = response.data.map((user)=>{
+          return{
+            _id: user.id,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+            address: user.address,
+            wish_list: user.wish_list,
+            story: user.story
+          };
+        });
+        setUserData(newUsers)
+      })
+      .catch((error)=> {
+        console.log(error);
+      });
+  },[])
   return (
     <div className="App">
       <header className="App-header">
